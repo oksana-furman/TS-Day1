08c589812c2f49ffce763febff8ed00ac8446693
[oksana-furman] <[oksan9a.furman@gmail.com]>
2023-02-15 14:21:24 +0100

    finish basic exercises

--- Oksana/script.js
+++ Oksana/script.js
@@ -1,39 +1,48 @@
 "use strict";
 //basic exercise 1
 // let numArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// let result1: any = document.getElementById("result1");
+// let result: any = document.getElementById("result");
 // for(let i=0; i<numArr.length; i++) {
 //     console.log(numArr[i]);
-//     result1.innerHTML += "<br>" + numArr[i] + "<br>";
+//     result.innerHTML += "<hr>" + numArr[i] + "<br>";
 //     for(let v=0; v<numArr.length; v++){
-//        console.log(numArr[i] + " x " + numArr[v] + " = " + numArr[i]*numArr[v]); 
-//        result1.innerHTML += numArr[i] + " x " + numArr[v] + " = " + numArr[i]*numArr [v] + "<br>";
+//        console.log(numArr[i] + " x " + numArr[v] + " = " + numArr[i] * numArr[v]); 
+//        result.innerHTML += numArr[i] + " x " + numArr[v] + " = " + numArr[i] * numArr [v] + "<br>";
 //     } 
 // }
 //end of basic exercise 1
 //basic exercise 3
-let names = ["John", "Mary", "Paul", "Bill", "Emma", "Christine", "Tom"];
-let result13 = document.getElementById("result13");
-let result23 = document.getElementById("result23");
-let result33 = document.getElementById("result33");
-for (let [i, val] of Object.entries(names)) {
-    // console.log(i);
-    result13.innerHTML += i + "<br>";
-}
-for (let [i, val] of Object.entries(names)) {
-    // console.log(val);
-    result23.innerHTML += val + "<br>";
-}
-for (let [i, val] of Object.entries(names)) {
-    // console.log(i, val);
-    result33.innerHTML += i + " " + val + "<br>";
-}
+// let names: string [] = ["John", "Mary", "Paul", "Bill", "Emma", "Christine", "Tom"];
+// let result13: any = document.getElementById("result13") as HTMLElement;
+// let result23: any = document.getElementById("result23") as HTMLElement;
+// let result33: any = document.getElementById("result33") as HTMLElement;
+// for(let [i, val] of Object.entries(names)){
+//     // console.log(i);
+//     result13.innerHTML += i + "<br>";
+// }
+// for(let [i, val] of Object.entries(names)){
+//     // console.log(val);
+//     result23.innerHTML += val + "<br>";
+// }
+// for(let [i, val] of Object.entries(names)){
+//     // console.log(i, val);
+//     result33.innerHTML += i + " " + val + "<br>";
+// }
 //end of basic exercise 3
 //basic exercise 2
-// let person: {}[] = [
-//     {fName: "Oksana", lName: "Furman"}
-// ];
-// for(let val of person){
-//     console.log(val.fName + " " + val.lName);
-// }
+let person = [
+    { fName: "Oksana", lName: "Furman" }
+];
+let result1 = document.getElementById("result1");
+let result2 = document.getElementById("result2");
+let result3 = document.getElementById("result3");
+for (let i = 0; i < 10; i++) {
+    console.log(person[0].fName + " " + person[0].lName);
+    result1.innerHTML += person[0].fName + " " + person[0].lName + "<br>";
+    result2.innerHTML += person[0].fName + "<br>";
+    setTimeout(() => {
+        result3.innerHTML += person[0].lName + "<br>";
+        console.log(person[0].lName);
+    }, 5000);
+}
 //end of basic exercise 2
--- Oksana/script.ts
+++ Oksana/script.ts
@@ -1,42 +1,53 @@
 //basic exercise 1
 // let numArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// let result1: any = document.getElementById("result1");
+// let result: any = document.getElementById("result");
+
 // for(let i=0; i<numArr.length; i++) {
 //     console.log(numArr[i]);
-//     result1.innerHTML += "<br>" + numArr[i] + "<br>";
+//     result.innerHTML += "<hr>" + numArr[i] + "<br>";
 //     for(let v=0; v<numArr.length; v++){
-//        console.log(numArr[i] + " x " + numArr[v] + " = " + numArr[i]*numArr[v]); 
-//        result1.innerHTML += numArr[i] + " x " + numArr[v] + " = " + numArr[i]*numArr [v] + "<br>";
+//        console.log(numArr[i] + " x " + numArr[v] + " = " + numArr[i] * numArr[v]); 
+//        result.innerHTML += numArr[i] + " x " + numArr[v] + " = " + numArr[i] * numArr [v] + "<br>";
 //     } 
 // }
 //end of basic exercise 1
 
-//basic exercise 3
-let names: string [] = ["John", "Mary", "Paul", "Bill", "Emma", "Christine", "Tom"];
-let result13: any = document.getElementById("result13") as HTMLElement;
-let result23: any = document.getElementById("result23") as HTMLElement;
-let result33: any = document.getElementById("result33") as HTMLElement;
 
-for(let [i, val] of Object.entries(names)){
-    // console.log(i);
-    result13.innerHTML += i + "<br>";
-}
-for(let [i, val] of Object.entries(names)){
-    // console.log(val);
-    result23.innerHTML += val + "<br>";
-}
-for(let [i, val] of Object.entries(names)){
-    // console.log(i, val);
-    result33.innerHTML += i + " " + val + "<br>";
-}
+//basic exercise 3
+// let names: string [] = ["John", "Mary", "Paul", "Bill", "Emma", "Christine", "Tom"];
+// let result13: any = document.getElementById("result13") as HTMLElement;
+// let result23: any = document.getElementById("result23") as HTMLElement;
+// let result33: any = document.getElementById("result33") as HTMLElement;
+
+// for(let [i, val] of Object.entries(names)){
+//     // console.log(i);
+//     result13.innerHTML += i + "<br>";
+// }
+// for(let [i, val] of Object.entries(names)){
+//     // console.log(val);
+//     result23.innerHTML += val + "<br>";
+// }
+// for(let [i, val] of Object.entries(names)){
+//     // console.log(i, val);
+//     result33.innerHTML += i + " " + val + "<br>";
+// }
 //end of basic exercise 3
 
-//basic exercise 2
-// let person: {}[] = [
-//     {fName: "Oksana", lName: "Furman"}
-// ];
-// for(let val of person){
-//     console.log(val.fName + " " + val.lName);
 
-// }
+//basic exercise 2
+let person: {fName: string, lName: string}[] = [
+    {fName: "Oksana", lName: "Furman"}
+];
+let result1: any = document.getElementById("result1") as HTMLElement;
+let result2: any = document.getElementById("result2") as HTMLElement;
+let result3: any = document.getElementById("result3") as HTMLElement;
+for(let i=0; i<10; i++){
+    console.log(person[0].fName + " " + person[0].lName);
+    result1.innerHTML += person[0].fName + " " + person[0].lName + "<br>";
+    result2.innerHTML += person[0].fName + "<br>";
+    setTimeout(() => {
+        result3.innerHTML += person[0].lName + "<br>";
+        console.log(person[0].lName)
+      }, 5000);
+}
 //end of basic exercise 2
